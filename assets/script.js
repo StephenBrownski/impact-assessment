291d7887e3b3c099b6bcdcaeed7a4c9a690a9911
StephenBrownski <stephen@helloelva.com>
2026-08-29 17:10:19 -0600

    Simple check for testing.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
 jQuery(document).ready(function ($) {
   // add custom JS code
+
+  $(document.body).on('added_to_cart wc-blocks_added_to_cart', async (e, fragments, cart_hash, $button) => {
+    console.log('Added.');
+  });
+
+
+
+  $(document.body).on('removed_from_cart wc-blocks_removed_from_cart', async (e, fragments, cart_hash, $button) => {
+    console.log('Removed.');
+  });
 });
